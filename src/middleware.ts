0a7103a2f40a5e7e9d2154fe31cacf410864a020
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-10-25 21:39:02 +0300

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -67,6 +67,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.webmanifest).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.webmanifest|images/).*)",
   ],
 };
